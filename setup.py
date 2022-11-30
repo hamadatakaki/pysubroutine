c6168a8c2ec411647a095b71558a6db4ad1fd5a7
hamadatakaki <i16hamadat@gmail.com>
2022-12-01 00:09:16 +0900

    add: setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="kuragesubroutine",
+    version="0.0.1",
+    description="My Subroutine exporting modules.",
+    author="jellyfish_rumble",
+    packages=find_packages(),
+)
