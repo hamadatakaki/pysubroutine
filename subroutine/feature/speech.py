e602f500d123fec40273ea8a1d78a2671afe5879
hamadatakaki <i16hamadat@gmail.com>
2022-12-04 17:24:38 +0900

    update: Speech feature object

--- subroutine/feature/speech.py
+++ subroutine/feature/speech.py
@@ -1,6 +1,10 @@
 import dataclasses
 from typing import Any, Optional, Union
 
+import sys
+import abc
+
+
 import librosa
 import numpy as np
 import pysptk
@@ -11,172 +15,200 @@ FeatureObject = Union[np.ndarray, int, float]
 
 
 @dataclasses.dataclass
-class LibrosaFeature(object):
+class BaseSpeechFeature(abc.ABC):
     config: dict[str, Any]
-    waveform: np.ndarray
-    spec: Optional[np.ndarray] = dataclasses.field(default=None, init=False)
-    melspec: Optional[np.ndarray] = dataclasses.field(default=None, init=False)
-    # features: list[str] = dataclasses.field(default=["spec", "melspec"], init=False)
+    waveform: Optional[np.ndarray] = dataclasses.field(default=None)
 
     def __post_init__(self):
-        self.waveform = self.waveform.astype(np.float64)
+        if self.waveform is not None:
+            self.waveform = self.waveform.astype(np.float64)
+
         self.assert_config()
 
     def assert_config(self):
-        config_keys = [
+        if self.config_keys is None:
+            raise NotImplementedError("Self::config_keys (list[str])")
+
+        exist_keys = [key in self.config for key in self.config_keys]
+
+        err = False
+        for res, key in zip(exist_keys, self.config_keys):
+            if not res:
+                err = True
+                print(f"[warning] config not have key: {key}", file=sys.stderr)
+
+        if err:
+            raise RuntimeError(
+                f"{self.__class__.__name__}::assert_config - some keys not exist."
+            )
+
+
+@dataclasses.dataclass
+class LibrosaFeature(BaseSpeechFeature):
+    features: list[str] = dataclasses.field(
+        default_factory=lambda: ["spectrogram", "mel_spectrogram"], init=False
+    )
+    config_keys: list[str] = dataclasses.field(
+        default_factory=lambda: [
             "fft_length",
             "hop_length",
             "sampling_rate",
             "win_length",
             "window",
             "num_mels",
-        ]
-        exist_keys = [key in self.config for key in config_keys]
+        ],
+        init=False,
+    )
 
-        err = False
-        for res, key in zip(exist_keys, config_keys):
-            if not res:
-                err = True
-                print(f"assert_config > key not exist: {key}")
-
-        assert not err, "LibrosaFeature::assert_config - some keys not exist."
+    _spec: Optional[np.ndarray] = dataclasses.field(default=None, init=False)
+    _melspec: Optional[np.ndarray] = dataclasses.field(default=None, init=False)
 
     def spectrogram(self) -> np.ndarray:
-        if self.spec is None:
+        if self._spec is None:
             fft_length = self.config["fft_length"]
             hop_length = self.config["hop_length"]
             win_length = self.config["win_length"]
 
-            self.spec = librosa.stft(
+            self._spec = librosa.stft(
                 self.waveform,
                 n_fft=fft_length,
                 hop_length=hop_length,
                 win_length=win_length,
             )
 
-        return self.spec
+        return self._spec
 
     def mel_spectrogram(self) -> np.ndarray:
-        if self.melspec is None:
+        if self._melspec is None:
             sr = self.config["sampling_rate"]
             fft_length = self.config["fft_length"]
             n_mels = self.config["num_mels"]
 
             spec = self.spectrogram()
             melfb = librosa.filters.mel(sr=sr, n_fft=fft_length, n_mels=n_mels)
-            self.melspec = librosa.amplitude_to_db(
+            self._melspec = librosa.amplitude_to_db(
                 np.dot(melfb, np.abs(spec)), ref=np.max
             )
 
-        return self.melspec
+        return self._melspec
 
-    def to_dict(self) -> dict[str, FeatureObject]:
-        return {"spec": self.spectrogram(), "melspec": self.mel_spectrogram()}
+    def get_features(
+        self, waveform: Optional[np.ndarray] = None
+    ) -> dict[str, FeatureObject]:
+        if waveform is not None:
+            self.waveform = waveform.astype(np.float64)
+
+        if self.waveform is None:
+            raise RuntimeError("LibrosaFeature::fet_features - Waveform is None")
+
+        return {
+            "spectrogram": self.spectrogram(),
+            "mel_spectrogram": self.mel_spectrogram(),
+        }
 
 
 @dataclasses.dataclass
-class WorldFeature(object):
-    config: dict[str, Any]
-    waveform: np.ndarray
-    f0: Optional[np.ndarray] = dataclasses.field(default=None, init=False)
-    timeaxis: Optional[np.ndarray] = dataclasses.field(default=None, init=False)
-    lf0: Optional[np.ndarray] = dataclasses.field(default=None, init=False)
-    vuv: Optional[np.ndarray] = dataclasses.field(default=None, init=False)
-    spec: Optional[np.ndarray] = dataclasses.field(default=None, init=False)
-    mgc: Optional[np.ndarray] = dataclasses.field(default=None, init=False)
-    bap: Optional[np.ndarray] = dataclasses.field(default=None, init=False)
+class WorldFeature(BaseSpeechFeature):
+    features: list[str] = dataclasses.field(
+        default_factory=lambda: [
+            "log_f0",
+            "vuv_flag",
+            "mel_cepstrum",
+            "band_aperiodicity",
+        ],
+        init=False,
+    )
+    config_keys: list[str] = dataclasses.field(
+        default_factory=lambda: ["frame_period", "sampling_rate", "mgc_order"],
+        init=False,
+    )
 
-    def __post_init__(self):
-        self.waveform = self.waveform.astype(np.float64)
-        self.assert_config()
-
-    def assert_config(self):
-        config_keys = [
-            "frame_period",
-            "sampling_rate",
-            "mgc_order",
-        ]
-        exist_keys = [key in self.config for key in config_keys]
-
-        err = False
-        for res, key in zip(exist_keys, config_keys):
-            if not res:
-                err = True
-                print(f"assert_config > key not exist: {key}")
-
-        assert not err, "WorldFeature::assert_config - some keys not exist."
+    _f0: Optional[np.ndarray] = dataclasses.field(default=None, init=False)
+    _timeaxis: Optional[np.ndarray] = dataclasses.field(default=None, init=False)
+    _lf0: Optional[np.ndarray] = dataclasses.field(default=None, init=False)
+    _vuv: Optional[np.ndarray] = dataclasses.field(default=None, init=False)
+    _spec: Optional[np.ndarray] = dataclasses.field(default=None, init=False)
+    _mgc: Optional[np.ndarray] = dataclasses.field(default=None, init=False)
+    _bap: Optional[np.ndarray] = dataclasses.field(default=None, init=False)
 
     def f0_and_timeaxis(self) -> tuple[np.ndarray, np.ndarray]:
-        if self.f0 is None or self.timeaxis is None:
+        if self._f0 is None or self._timeaxis is None:
             sr = self.config["sampling_rate"]
             frame_period = self.config["frame_period"]
 
             f0, timeaxis = pyworld.dio(self.waveform, sr, frame_period=frame_period)
-            self.f0 = f0
-            self.timeaxis = timeaxis
+            self._f0 = f0
+            self._timeaxis = timeaxis
 
-        assert self.f0 is not None
-        assert self.timeaxis is not None
+        assert self._f0 is not None
+        assert self._timeaxis is not None
 
-        return (self.f0, self.timeaxis)
+        return (self._f0, self._timeaxis)
 
-    def continuous_logarithmic_f0(self) -> np.ndarray:
-        if self.lf0 is None:
+    def log_f0(self) -> np.ndarray:
+        if self._lf0 is None:
             f0, _ = self.f0_and_timeaxis()
 
             f0[f0 < 0] = 0
             lf0 = f0.copy()
             nonzero = np.nonzero(f0)
             lf0[nonzero] = np.log(f0[nonzero])
-            clf0 = self.__interp1d(lf0)
-            self.lf0 = clf0.reshape((-1, 1))
+            self._lf0 = self.__interp1d(lf0).reshape((-1, 1))
 
-        return self.lf0
+        return self._lf0
 
     def vuv_flag(self) -> np.ndarray:
-        if self.vuv is None:
+        if self._vuv is None:
             f0, _ = self.f0_and_timeaxis()
             vuv = (f0 > 0).astype(np.float64)
-            self.vuv = vuv.reshape((-1, 1))
+            self._vuv = vuv.reshape((-1, 1))
 
-        return self.vuv
+        return self._vuv
 
     def spectrogram(self) -> np.ndarray:
-        if self.spec is None:
+        if self._spec is None:
             sr = self.config["sampling_rate"]
 
             f0, timeaxis = self.f0_and_timeaxis()
-            self.spec = pyworld.cheaptrick(self.waveform, f0, timeaxis, sr)
+            self._spec = pyworld.cheaptrick(self.waveform, f0, timeaxis, sr)
 
-        return self.spec
+        return self._spec
 
-    def mel_gemeralized_cepstrum(self) -> np.ndarray:
-        if self.mgc is None:
+    def mel_cepstrum(self) -> np.ndarray:
+        if self._mgc is None:
             sr = self.config["sampling_rate"]
             mgc_order = self.config["mgc_order"]
             alpha = pysptk.util.mcepalpha(sr)
 
             spec = self.spectrogram()
-            self.mgc = pysptk.sp2mc(spec, mgc_order, alpha)
+            self._mgc = pysptk.sp2mc(spec, mgc_order, alpha)
 
-        return self.mgc
+        return self._mgc
 
     def band_aperiodicity(self) -> np.ndarray:
-        if self.bap is None:
+        if self._bap is None:
             sr = self.config["sampling_rate"]
 
             f0, timeaxis = self.f0_and_timeaxis()
             ap = pyworld.d4c(self.waveform, f0, timeaxis, sr)
-            self.bap = pyworld.code_aperiodicity(ap, sr)
+            self._bap = pyworld.code_aperiodicity(ap, sr)
 
-        return self.bap
+        return self._bap
 
-    def to_dict(self) -> dict[str, FeatureObject]:
+    def get_features(
+        self, waveform: Optional[np.ndarray] = None
+    ) -> dict[str, FeatureObject]:
+        if waveform is not None:
+            self.waveform = waveform.astype(np.float64)
+
+        if self.waveform is None:
+            raise RuntimeError("WorldFeature::fet_features - Waveform is None")
+
         return {
-            "lf0": self.continuous_logarithmic_f0(),
-            "vuv": self.vuv_flag(),
-            "mgc": self.mel_gemeralized_cepstrum(),
-            "bap": self.band_aperiodicity(),
+            "log_f0": self.log_f0(),
+            "vuv_flag": self.vuv_flag(),
+            "mel_cepstrum": self.mel_cepstrum(),
+            "band_aperiodicity": self.band_aperiodicity(),
         }
 
     def __interp1d(self, f0: np.ndarray, kind: str = "slinear") -> np.ndarray:
